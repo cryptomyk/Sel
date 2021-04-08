2bdceb086819fbd05e940192c24d0a67f46d034b
cryptomyk <58111971+cryptomyk@users.noreply.github.com>
2021-04-08 17:24:02 +0530

    Update Test1.java

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -7,7 +8,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -24,73 +27,88 @@ public class Test1  {
 	static	 ExtentSparkReporter extentSparkReporter= new ExtentSparkReporter("reports/extent/index.html");
 	ExtentReports extent;
 	ExtentTest logger;
-
+	ExcelConfig config;
+	WebDriver driver;
 	
 	
-	public void startReport(){
-		//ExtentReports(String filePath,Boolean replaceExisting) 
-		//filepath - path of the file, in .htm or .html format - path where your report needs to generate. 
-		//replaceExisting - Setting to overwrite (TRUE) the existing file or append to it
-		//True (default): the file will be replaced with brand new markup, and all existing data will be lost. Use this option to create a brand new report
-		//False: existing data will remain, new tests will be appended to the existing report. If the the supplied path does not exist, a new file will be created.
-		extent = new ExtentReports ();
-		//extent.addSystemInfo("Environment","Environment Name")
 		
-              extent.attachReporter(extentSparkReporter);
-	}
-		
-	//@Test
+	@BeforeMethod
 	public void info(){
-		//extent.startTest("TestCaseName", "Description")
-		//TestCaseName – Name of the test
-		//Description – Description of the test
-		//Starting test
-		
-		Assert.assertTrue(true);
-		//To generate the log when the test case is passed
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\minbal521\\Documents\\Chrome driver\\chromedriver.exe");
+		driver = new ChromeDriver();
 		
 	}
 	
 	
 	 
 		@Test
-		public  void test1() throws InterruptedException {
+		public  void test1() throws InterruptedException, IOException {
 			extent = new ExtentReports ();
 			//extent.addSystemInfo("Environment","Environment Name")
 			
-	              
+			config = new ExcelConfig();    
 			
-			System.setProperty("webdriver.chrome.driver","C:\\Users\\minbal521\\Documents\\Chrome driver\\chromedriver.exe");
-			WebDriver driver = new ChromeDriver();
+			
+			
 		
 			driver.get("https://google.com");
 			System.out.println("In google.com");
 			logger = extent.createTest("Test1");
+			if(driver.getTitle().equals("Google")) {
+			config.passData("Moved to google.com successfully",  1, 1);
+			config.passData("PASS",  1, 2);}
+			else {
+				config.passData("NOT Moved to google.com successfully",  1, 1);
+			config.passData("FAIL",  1, 2);}
+			
 			logger.log(Status.INFO,"In google.com");
 			driver.findElement(By.name("q")).sendKeys("java");
 			System.out.println("Entering java");
 			logger.log(Status.INFO,"Entering java");
+			if(driver.getTitle().equals("Google")) {
+				config.passData("Entering java",  2, 1);
+				config.passData("PASS",  2, 2);}
+				
+			else {
+				config.passData("not Entering java",  2, 1);
+			config.passData("FAIL",  2, 2);}
+			
+			
+			
 			driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
 			System.out.println("pressing enter key");
 			logger.log(Status.INFO,"pressing enter key");
+			
+			if (driver.getTitle().equals("java - Google Search1")){
+				config.passData("pressing enter key",  3, 1);
+				config.passData("PASS",  3, 2);}
+				
+			else {
+				config.passData("not pressing enter key",  3, 1);
+			config.passData("FAIL",  3, 2);}
+				
+				
+			
 			extent.attachReporter(extentSparkReporter);
 			extent.flush();
 			Thread.sleep(2000);
+			
+			
+			
+		}
+		
+		@AfterMethod
+		public void onTearDown() {
+			
 			
 			driver.quit();
 			
 		}
 		
-		@AfterClass
-		public void onTearDown() {
-			
-			
-		
-			
-		}
-		
 		
 	}
+
+
 
 
 
